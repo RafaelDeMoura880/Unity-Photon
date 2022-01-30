ad629703912e627884cd4e8bc522eb54abe740e0
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-01-30 19:38:15 -0300

    Added player asset package and movement script

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float Velocidade = 7;
+    public float VelocidadeGiro = 3;
+
+    Rigidbody rb;
+    float inputH;
+    float inputV;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //caminhada
+        Vector3 novaVelocidade = transform.forward * inputV * Velocidade;
+        //manter a velocidade em y (para não afetar gravidade)
+        novaVelocidade.y = rb.velocity.y;
+        rb.velocity = novaVelocidade;
+
+        //giro
+        rb.angularVelocity = new Vector3(0, inputH * VelocidadeGiro, 0);
+    }
+}
