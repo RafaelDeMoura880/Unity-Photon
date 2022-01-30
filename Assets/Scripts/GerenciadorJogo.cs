c176a524238790392d4642fdc9deb6d91480b1fb
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-01-30 19:58:32 -0300

    Added player instantiation and test build

--- Assets/Scripts/GerenciadorJogo.cs
+++ Assets/Scripts/GerenciadorJogo.cs
@@ -6,6 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class GerenciadorJogo : MonoBehaviourPunCallbacks
 {
+    public GameObject PrefabJogador;
+    
+    private void Start()
+    {
+        if(PrefabJogador != null)
+        {
+            Vector3 posicao = new Vector3(0, 5, 0);
+            PhotonNetwork.Instantiate(PrefabJogador.name, posicao, Quaternion.identity);
+        }
+    }
+
     public void SairDaSala()
     {
         PhotonNetwork.LeaveRoom();
