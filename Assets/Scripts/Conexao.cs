f5658ce0e4b05e6c48e8fe070463f5115845eff6
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-01-30 18:40:40 -0300

    Initial commit

---
+++ Assets/Scripts/Conexao.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Conexao : MonoBehaviourPunCallbacks
+{
+    string versao = "1";
+    bool conectando = false;
+    
+    private void Start()
+    {
+        //Conectar();
+    }
+
+    public void Conectar()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            conectando =PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = versao;
+        }
+        else
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        if (conectando)
+        {
+            Debug.Log("Conectado ao servidor. A lógica do jogo pode iniciar aqui");
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Desconectado. Causa: " + cause);
+        conectando = false;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Juntou-se a uma sala de jogo!");
+        //aqui vai chamar a cena de jogo
+        PhotonNetwork.LoadLevel("cenario-jogo1");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Falhou ao tentar juntar a uma sala de jogo code: " 
+            + returnCode + "; msg: " + message + ". Vamos criar uma sala"); 
+        PhotonNetwork.CreateRoom("sala-jogo", new RoomOptions() { MaxPlayers = 4 });
+    }
+}
---
+++ Assets/Scripts/GerenciadorJogo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GerenciadorJogo : MonoBehaviourPunCallbacks
+{
+    public void SairDaSala()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Conexao");
+    }
+}
