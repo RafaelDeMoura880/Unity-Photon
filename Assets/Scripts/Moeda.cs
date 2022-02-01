05c00f06f86b6cba34ffd4c86a2c058505a18696
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-01-31 22:31:01 -0300

    Added coin pickup

---
+++ Assets/Scripts/Moeda.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class Moeda : MonoBehaviourPun
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (PhotonNetwork.IsMasterClient && other.CompareTag("Player"))
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+}
