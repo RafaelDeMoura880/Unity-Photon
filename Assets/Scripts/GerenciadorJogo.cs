3c348c5f77de6e2b8b0c7360731ac1765203b248
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-22 20:38:27 -0300

    Implementing DefineJogador() method

--- Assets/Scripts/GerenciadorJogo.cs
+++ Assets/Scripts/GerenciadorJogo.cs
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 public class GerenciadorJogo : MonoBehaviourPunCallbacks
 {
     public GameObject PrefabJogador;
+    public double TempoMaximo = 45;
+
+    bool jogoEmAndamento;
     
     private void Start()
     {
@@ -15,6 +18,28 @@ public class GerenciadorJogo : MonoBehaviourPunCallbacks
             Vector3 posicao = new Vector3(0, 5, 0);
             PhotonNetwork.Instantiate(PrefabJogador.name, posicao, Quaternion.identity);
         }
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            ExitGames.Client.Photon.Hashtable propriedades =
+                new ExitGames.Client.Photon.Hashtable();
+            propriedades.Add("TempoInicio", PhotonNetwork.Time);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(propriedades);
+            jogoEmAndamento = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient || !jogoEmAndamento)
+            return;
+
+        DefineGanhador();
+    }
+
+    void DefineGanhador()
+    {
+        
     }
 
     public void SairDaSala()
--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,24 @@ public class Hud : MonoBehaviour
 
     private void Update()
     {
-        Jogador[] jogadores = GameObject.FindObjectsOfType<Jogador>();
-        string textoPontuacao = "";
-        foreach (Jogador j in jogadores)
-            textoPontuacao += j.Pontuacao + "; ";
-        TxtPontuacoes.text = textoPontuacao;
+        if(PhotonNetwork.CurrentRoom.CustomProperties["TempoInicio"] != null)
+        {
+            //recuperando o valor do tempo inicial e subtraindo do tempo atual
+            double tempo = PhotonNetwork.Time - 
+                (double)PhotonNetwork.CurrentRoom.CustomProperties["TempoInicio"];
+            //ToString("0") é usado para desprezar casas decimais
+            TxtPontuacoes.text = tempo.ToString("0");
+        }
     }
+
+    //Previous Update counting player score
+
+    //private void Update()
+    //{
+    //    Jogador[] jogadores = GameObject.FindObjectsOfType<Jogador>();
+    //    string textoPontuacao = "";
+    //    foreach (Jogador j in jogadores)
+    //        textoPontuacao += j.Pontuacao + "; ";
+    //    TxtPontuacoes.text = textoPontuacao;
+    //}
 }
