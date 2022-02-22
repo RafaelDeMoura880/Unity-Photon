1283198350451040c920a500ccdeb13fa1b87c40
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-22 19:37:21 -0300

    Added player coin count

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    public Jogador MeuPlayer;
+    public Text TxtPontuacao;
+
+    private void Update()
+    {
+        TxtPontuacao.text = "$ " + MeuPlayer.Pontuacao;
+    }
+}
